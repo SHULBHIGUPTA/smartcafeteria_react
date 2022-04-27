3db68cfd027181131d302ea1a1437d3815c03823
SHULBHIGUPTA <shulbhi18@gmail.com>
2022-04-27 07:30:19 -0700

    shulbhi commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+import * as XLSX from "xlsx";
 
 function App() {
+  const [items, setItems] = useState([]);
+  
+  const readExcel = (file) => {
+    const promise = new Promise((resolve, reject) => {
+      const fileReader = new FileReader();
+      fileReader.readAsArrayBuffer(file);
+
+      fileReader.onload = (e) => {
+        const bufferArray = e.target.result;
+
+        const wb = XLSX.read(bufferArray, { type: "buffer" });
+
+        const wsname = wb.SheetNames[0];
+
+        const ws = wb.Sheets[wsname];
+
+        const data = XLSX.utils.sheet_to_json(ws);
+
+        resolve(data);
+      };
+
+      fileReader.onerror = (error) => {
+        reject(error);
+      };
+    });
+
+    promise.then((d) => {
+      console.log(d);
+      setItems(d);
+    });
+  };
+
+const imgstyle = {width:"30vw", height: "30vh"}
+const containerstyle = {width:"50vw"}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+       <nav className="bg-info p-1 text-center">
+              <h3>Smart Cafeteria</h3>
+        </nav>
+        <div className="input-group">
+        <input type="file" className="form-control " id="inputGroupFile04" aria-describedby="inputGroupFileAddon04" aria-label="Upload" 
+        onChange={(e) => {
+          const file = e.target.files[0];
+          readExcel(file);
+        }}/>
+        </div>
+    {items.map((d) => (
+        <table className="table container bg-light text-center" style={containerstyle}>
+          <thead>
+            <tr >
+              <th scope="col text-center">Month</th>
+              <th scope="col text-center">Week</th>
+              <th scope="col text-center">Days</th>
+              <th scope="col text-center">Food menu</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr key={d.DATE}>
+              <td className="text-center">{d.MONTH}</td>
+              <td className="text-center">{d.WEEK}</td>
+              <td className="text-center">{d.DATE}</td>
+              <td>
+                <img src={d.IMAGE} style={imgstyle}/>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      ))}
     </div>
-  );
+  )
 }
 
 export default App;
